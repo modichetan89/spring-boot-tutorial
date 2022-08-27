61852d0eacdf606bf058e5864fdff2a63f5c27ce
Chetan <modichetan89@gmail.com>
2022-08-27 18:39:16 +0530

    Feature POST and GET Service added

---
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -0,0 +1,43 @@
+package com.dailycodebuffer.Springboot.tutorial.controller;
+
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    //Below tags are for Swagger and Open API documentation
+    @Operation(summary = "Save new department")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Added new department",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Department.class)) }),
+            @ApiResponse(responseCode = "400", description = "Client error",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content) })
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+}
---
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.dailycodebuffer.Springboot.tutorial.service;
+
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
+    }
+}
