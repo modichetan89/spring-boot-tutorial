50e42e5d81d102c190eaa9e783c609318a23135a
Chetan <modichetan89@gmail.com>
2023-01-09 18:55:40 +0530

    Added Get Delete Put operation

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
@@ -8,4 +8,10 @@ public interface DepartmentService {
     public Department saveDepartment(Department department);
 
     public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartmentById(Long departmentId, Department department);
 }
--- src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
@@ -21,5 +22,36 @@ public class DepartmentServiceImpl implements DepartmentService{
     @Override
     public List<Department> fetchDepartmentList() {
         return departmentRepository.findAll();
+    }
+
+    @Override
+    public Department fetchDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId).get();
+    }
+
+    @Override
+    public void deleteDepartmentById(Long departmentId) {
+        departmentRepository.deleteById(departmentId);
+    }
+
+    @Override
+    public Department updateDepartmentById(Long departmentId, Department department) {
+        Department depDB = departmentRepository.findById(departmentId).get();
+        if(Objects.nonNull(department.getDepartmentName()) &&
+        !"".equalsIgnoreCase(department.getDepartmentName())){
+            depDB.setDepartmentName(department.getDepartmentName());
+        }
+
+        if(Objects.nonNull(department.getDepartmentCode()) &&
+                !"".equalsIgnoreCase(department.getDepartmentCode())){
+            depDB.setDepartmentCode(department.getDepartmentCode());
+        }
+
+        if(Objects.nonNull(department.getDepartmentAddress()) &&
+                !"".equalsIgnoreCase(department.getDepartmentAddress())){
+            depDB.setDepartmentAddress(department.getDepartmentAddress());
+        }
+
+        return departmentRepository.save(depDB);
     }
 }
