1ed257ca881644295251480244148b39d371948f
Chetan <modichetan89@gmail.com>
2023-01-10 18:42:29 +0530

    Hibernate Validation & Loggers & Lombok

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -1,26 +1,46 @@
 package com.dailycodebuffer.Springboot.tutorial.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
 
+    @NotBlank(message = "Please add department name")
+//    @Length(max = 5, min =1)
+//    @Size(max = 10, min = 0)
+//    @Email
+//    @Positive
+//    @Negative
+//    @PositiveOrZero
+//    @NegativeOrZero
+//    @Future
+//    @Past
+//    @FutureOrPresent
+//    @PastOrPresent
     private String departmentName;
-
     private String departmentAddress;
     private String departmentCode;
 
 
+/*  Comment below code as we are using Lombok which will do this work for us with help of annotations
     public Long getDepartmentId() {
         return departmentId;
     }
@@ -72,4 +92,7 @@ public class Department {
                 ", departmentCode='" + departmentCode + '\'' +
                 '}';
     }
+
+    //commented above as not needed due to lombok annotations
+ */
 }
